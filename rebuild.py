b53b944e67c6486dd7330823f17d0453c9402f34
Will Norris <will@willnorris.com>
2014-03-19 15:21:34 -0700

    update rebuild.py to build dash TOC tags
    we use html5lib with BeautifulSoup, since it seems to be the least bad
    at munging the rest of the document.  html.parser and lxml both break
    things to the point that the header at the top of each RFC does not
    display correctly.  html5lib still munges some things (like reformating
    markup and reordering attributes), but they do not appear to affect the
    display of the page.
    
    also update Info.plist to look for tables of content.

--- rebuild.py
+++ rebuild.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, re, sqlite3
+import os, re, sqlite3, urllib
 from bs4 import BeautifulSoup, NavigableString, Tag 
 
 db = sqlite3.connect('rfc.docset/Contents/Resources/docSet.dsidx')
@@ -17,9 +17,8 @@ HTML_DIR = os.path.join('tools.ietf.org', 'html')
 for filename in os.listdir(os.path.join(DOCUMENTS_DIR, HTML_DIR)):
     page = open(os.path.join(DOCUMENTS_DIR, HTML_DIR, filename)).read()
 
-    soup = BeautifulSoup(page)
+    soup = BeautifulSoup(page, 'html5lib')
 
-    any = re.compile('.*')
     for tag in soup.find_all('title'):
         name = tag.text.strip()
 
@@ -28,6 +27,25 @@ for filename in os.listdir(os.path.join(DOCUMENTS_DIR, HTML_DIR)):
             cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Guide', path))
             print 'name: %s, path: %s' % (name, path)
 
+    for tag in soup.find_all('span'):
+        if tag.get('class'):
+            for c in tag.get('class'):
+                if c in ['h2', 'h3', 'h4', 'h5', 'h6']:
+                    dashAnchor = tag.find('a', class_='dashAnchor')
+                    if dashAnchor:
+                        continue
+
+                    text = tag.text.strip()
+                    text = text.replace(u'\xa0', u' ') # convert non-breaking space to regular space
+
+                    #print 'adding toc tag for section: %s' % text
+                    name = '//apple_ref/Section/' + urllib.quote(text, '')
+                    dashAnchor = BeautifulSoup('<a name="%s" class="dashAnchor"></a>' % name).a
+                    tag.insert(0, dashAnchor)
+
+    fp = open(os.path.join(DOCUMENTS_DIR, HTML_DIR, filename), 'w')
+    fp.write(str(soup))
+    fp.close()
 
 db.commit()
 db.close()
